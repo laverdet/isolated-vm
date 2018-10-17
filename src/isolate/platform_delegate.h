e7c507d813722aba59d01338a7522de1a9e90a39
Marcel Laverdet <marcel@laverdet.com>
2018-10-17 03:02:39 -0500

    Implement v8::TaskRunner API
    This fixes WebAssembly in nodejs v10.9.x and above. And maybe some other
    stuff.

--- src/isolate/platform_delegate.h
+++ src/isolate/platform_delegate.h
@@ -27,6 +27,8 @@ class PlatformDelegate : public v8::Platform {
 				std::unique_ptr<v8::Task> task;
 			public:
 				explicit TaskHolder(v8::Task* task) : task(task) {}
+				explicit TaskHolder(std::unique_ptr<v8::Task>&& task) : task(std::move(task)) {}
+				explicit TaskHolder(TaskHolder&& task) : task(std::move(task.task)) {}
 				void Run() final {
 					task->Run();
 				}
@@ -58,6 +60,50 @@ class PlatformDelegate : public v8::Platform {
 			v8::V8::InitializePlatform(&instance);
 		}
 
+		/**
+		 * ~ An Abridged History of v8's threading and task API ~
+		 *
+		 * 6.4.168:
+		 * - `GetForegroundTaskRunner(Isolate*)` and `GetBackgroundTaskRunner(Isolate*)` are introduced [c690f54d]
+		 *
+		 * 6.7.1:
+		 * - `GetBackgroundTaskRunner` renamed to `GetWorkerThreadsTaskRunner` [70222a9d]
+		 * - `CallOnWorkerThread(Task* task)` added [86b4b534]
+		 * - `ExpectedRuntime` parameter removed from `CallOnBackgroundThread(Task*, ExpectedRuntime)` [86b4b534]
+		 * - `CallBlockingTaskFromBackgroundThread(Task* task)` added [2600038d]
+		 *
+		 * 6.7.179:
+		 * - `CallOnWorkerThread` and `CallBlockingTaskOnWorkerThread` parameters both
+		 *   changed to `(std::unique_ptr<Task> task)` [1983f305]
+		 *
+		 * 6.8.117:
+		 * - `GetWorkerThreadsTaskRunner` removed [4ac96190]
+		 * - `CallDelayedOnWorkerThread(std::unique_ptr<Task> task, double delay_in_seconds)` added [4b13a22f]
+		 *
+		 * 6.8.242:
+		 * - `CallOnBackgroundThread`, `NumberOfWorkerThreads`, `GetForegroundTaskRunner`, and
+		 *   `CallOnWorkerThread` removed [8f6ffbfc]
+		 * - `CallDelayedOnWorkerThread` becomse pure virtual [8f6ffbfc]
+		 *
+		 * 7.1.263:
+		 * - `CallOnForegroundThread`, `CallDelayedOnForegroundThread`, and `CallIdleOnForegroundThread`
+		 *   deprecated
+		 *
+		 * 7.1.316:
+		 * - `PostNonNestableTask(std::unique_ptr<Task> task)` and `NonNestableTasksEnabled()` added to
+		 *   `v8::TaskRunner`
+		 *
+		 *
+		 * ~ Meanwhile in nodejs ~
+		 *
+		 * 10.0.0:
+		 * - Updates v8 to 6.6.346 but includes random internal changes from v8 6.7.x [2a3f8c3a]
+		 *
+		 * 10.9.0:
+		 * - Updates v8 to 6.8.275 but continues to include old API requirements from v8 v6.7.x [5fa3ffad]
+		 *
+		 */
+
 #if V8_AT_LEAST(6, 7, 1)
 		int NumberOfWorkerThreads() final {
 			return node_platform->NumberOfWorkerThreads();
@@ -73,6 +119,7 @@ class PlatformDelegate : public v8::Platform {
 		}
 
 		void CallOnBackgroundThread(v8::Task* task, ExpectedRuntime /* expected_runtime */) final {
+			// TODO: Properly count these tasks against isolate timers. How common are background tasks??
 			node_platform->CallOnWorkerThread(std::unique_ptr<v8::Task>(task));
 		}
 #else
@@ -82,17 +129,61 @@ class PlatformDelegate : public v8::Platform {
 #endif
 
 #if V8_AT_LEAST(6, 4, 168)
-		// These are the new APIs that will be used instead of `CallOn` functions, but as of now they
-		// are just placeholders. An explicit termination is invoked so that future versions of nodejs
-		// won't have undefined behavior. When these functions become active this whole platform thing
-		// will need to be refactored.
 		std::shared_ptr<v8::TaskRunner> GetBackgroundTaskRunner(v8::Isolate* /* isolate */) final {
-			std::terminate();
-			return {nullptr};
+			return node_platform->GetBackgroundTaskRunner(node_isolate);
 		}
 
-		std::shared_ptr<v8::TaskRunner> GetForegroundTaskRunner(v8::Isolate* /* isolate */) final {
-			std::terminate();
+	private:
+		// nb: The v8 documentation says that methods on this object may be called from any thread.
+		class ForegroundTaskRunner : public v8::TaskRunner {
+			private:
+				std::weak_ptr<IsolateHolder> holder;
+
+			public:
+				ForegroundTaskRunner(std::shared_ptr<IsolateHolder> holder) : holder(std::move(holder)) {}
+
+				void PostTask(std::unique_ptr<v8::Task> task) final {
+					auto ref = holder.lock();
+					if (ref) {
+						ref->ScheduleTask(std::make_unique<TaskHolder>(std::move(task)), false, false, true);
+					}
+				}
+
+				void PostDelayedTask(std::unique_ptr<v8::Task> task, double delay_in_seconds) final {
+					auto shared_task = std::make_shared<TaskHolder>(std::move(task));
+					auto holder = this->holder;
+					timer_t::wait_detached(delay_in_seconds * 1000, [holder, shared_task](void* next) {
+						auto ref = holder.lock();
+						if (ref) {
+							// Don't wake the isolate because that will affect the libuv ref/unref stuff. Instead,
+							// if this isolate is not running then whatever task v8 wanted to run will fire first
+							// thing next time the isolate is awake.
+							ref->ScheduleTask(std::make_unique<TaskHolder>(std::move(*shared_task)), false, false, true);
+						}
+						timer_t::chain(next);
+					});
+				}
+
+				void PostIdleTask(std::unique_ptr<v8::IdleTask> task) final {
+					std::terminate();
+				}
+
+				bool IdleTasksEnabled() final {
+					return false;
+				}
+		};
+
+	public:
+		std::shared_ptr<v8::TaskRunner> GetForegroundTaskRunner(v8::Isolate* isolate) final {
+			if (isolate == node_isolate) {
+				node_platform->GetForegroundTaskRunner(isolate);
+			} else if (isolate != tmp_isolate) {
+				auto s_isolate = IsolateEnvironment::LookupIsolate(isolate);
+				// We could further assert that IsolateEnvironment::GetCurrent() == s_isolate
+				assert(s_isolate);
+				// TODO: Don't make a new runner each time
+				return std::make_shared<ForegroundTaskRunner>(s_isolate);
+			}
 			return {nullptr};
 		}
 #endif
