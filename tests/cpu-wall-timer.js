838ffb4b31080a3619be09e98bc1fd06a2c9e85f
Marcel Laverdet <marcel@laverdet.com>
2018-12-28 19:06:53 +0000

    Fix brokenish cpu timer test

--- tests/cpu-wall-timer.js
+++ tests/cpu-wall-timer.js
@@ -1,5 +1,7 @@
 'use strict';
-let ivm = require('isolated-vm');
+const kTimeout = 100;
+const kTimeoutRange = 115;
+let ivm = require('../isolated-vm');
 let isolate = new ivm.Isolate;
 let context = isolate.createContextSync();
 
@@ -16,6 +18,10 @@ function hrToFloat(hr) {
 	return time;
 }
 
+function checkRange(val, min, max) {
+	return val <= max && val >= min;
+}
+
 // Synchronous timers
 context.global.setSync('defaultIsolateSpin', new ivm.Reference(spin));
 context.global.setSync('recurse', new ivm.Reference(function(code) {
@@ -23,48 +29,47 @@ context.global.setSync('recurse', new ivm.Reference(function(code) {
 }));
 isolate.compileScriptSync(''+ spin).runSync(context);
 isolate.compileScriptSync(`
-	spin(100);
-	defaultIsolateSpin.applySync(undefined, [ 100 ]);
-	recurse.applySync(undefined, [ 'spin(100);' ]);
+	spin(${kTimeout});
+	defaultIsolateSpin.applySync(undefined, [ ${kTimeout} ]);
+	recurse.applySync(undefined, [ 'spin(${kTimeout});' ]);
 `).runSync(context);
 
-if (Math.abs(hrToFloat(isolate.cpuTime)) - 200 > 20) {
+if (!checkRange(hrToFloat(isolate.cpuTime), kTimeout * 2, kTimeoutRange * 2)) {
 	console.log('cpu time wrong');
 }
-if (Math.abs(hrToFloat(isolate.wallTime)) - 300 > 20) {
+if (!checkRange(hrToFloat(isolate.wallTime), kTimeout * 3, kTimeoutRange * 3)) {
 	console.log('wall time wrong');
 }
 
 // Asynchronous time
 let initialCpu = hrToFloat(isolate.cpuTime);
+let lastCpu;
 let initialWall = hrToFloat(isolate.wallTime);
-isolate.compileScriptSync('spin(100); defaultIsolateSpin.applySync(undefined, [ 100 ] );').run(context);
-let d = Date.now() + 250;
+isolate.compileScriptSync(`spin(${kTimeout}); defaultIsolateSpin.applySync(undefined, [ ${kTimeout} ] );`).run(context);
+let d = Date.now() + kTimeoutRange * 2;
 let passedCpu = false;
-let passedWall = false;
-while (Date.now() < d && !(passedCpu && passedWall)) {
+while (Date.now() < d && !passedCpu) {
 	let cpuTime = hrToFloat(isolate.cpuTime);
-	if (initialCpu !== 0) {
-		if (cpuTime !== initialCpu) {
-			initialCpu = 0;
-		}
-	} else if (cpuTime > 300) {
+	// Ensures cpu is ticking up while the spin() loop runs in another thread
+	if (lastCpu === undefined) {
+		lastCpu = cpuTime;
+	} else if (lastCpu !== cpuTime) {
 		passedCpu = true;
 	}
+}
+if (!passedCpu) {
+	console.log('Async CPU time not updating');
+}
 
-	let wallTime = hrToFloat(isolate.wallTime);
-	if (initialWall !== 0) {
-		if (wallTime !== initialWall) {
-			initialWall = 0;
-		}
-	} else if (wallTime > 500) {
-		passedWall = true;
+d = Date.now() + kTimeout * 4;
+let interval = setInterval(function() {
+	if (Date.now() > d) {
+		clearInterval(interval);
 	}
-}
-if (!passedCpu || hrToFloat(isolate.cpuTime) > 350) {
-	console.log('async cpu failed');
-}
-if (!passedWall || hrToFloat(isolate.wallTime) > 550) {
-	console.log('async wall failed');
-}
-console.log('pass');
+	if (checkRange(hrToFloat(isolate.wallTime) - initialWall, kTimeout * 2, kTimeoutRange * 2)) {
+		clearInterval(interval);
+		if (checkRange(hrToFloat(isolate.cpuTime) - initialCpu, kTimeout, kTimeoutRange)) {
+			console.log('pass');
+		}
+	}
+}, 10);
