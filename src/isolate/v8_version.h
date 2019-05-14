6e211f55b8d513a4a20fcd8a81652531e14e2d1a
Marcel Laverdet <marcel@laverdet.com>
2019-05-14 02:31:44 -0500

    Fix incorrect V8_AT_LEAST macro

--- src/isolate/v8_version.h
+++ src/isolate/v8_version.h
@@ -16,7 +16,7 @@
 
 #define V8_AT_LEAST(major, minor, patch) (\
 	V8_MAJOR_VERSION > (major) || \
-	(V8_MAJOR_VERSION == (major) && V8_MINOR_VERSION >= (minor)) || \
+	(V8_MAJOR_VERSION == (major) && V8_MINOR_VERSION > (minor)) || \
 	(V8_MAJOR_VERSION == (major) && V8_MINOR_VERSION == (minor) && V8_PATCH_LEVEL >= (patch)) \
 )
 
