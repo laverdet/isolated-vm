3692d80ca653a69e9d96a6ba30802db635e558f4
Marcel Laverdet <marcel@laverdet.com>
2021-11-18 11:48:37 -0600

    Fix nodejs v16.11.0 ABI
    Fixes #264

--- src/isolate/v8_inspector_wrapper.h
+++ src/isolate/v8_inspector_wrapper.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "node_wrapper.h"
+#include "./v8_version.h"
 #if NODE_MODULE_VERSION >= 93
+#if V8_AT_LEAST(9, 4, 146)
+#include "v8_inspector/nodejs_v16.11.0.h"
+#else
 #include "v8_inspector/nodejs_v16.0.0.h"
+#endif
 #elif NODE_MODULE_VERSION >= 83
 #include "v8_inspector/nodejs_v14.0.0.h"
 #elif NODE_MODULE_VERSION >= 67
