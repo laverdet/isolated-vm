8e335994fadad5710642e5628a13a38066669d69
Xuguang Mei <meixg@foxmail.com>
2023-02-09 10:32:59 +0800

    fix: inspector example, message should be string

--- inspector-example.js
+++ inspector-example.js
@@ -32,7 +32,7 @@ wss.on('connection', function(ws) {
 	// Relay messages from frontend to backend
 	ws.on('message', function(message) {
 		try {
-			channel.dispatchProtocolMessage(message);
+			channel.dispatchProtocolMessage(message.toString());
 		} catch (err) {
 			// This happens if inspector session was closed unexpectedly
 			ws.close();
