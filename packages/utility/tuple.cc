634fda84ccec1dc33a9a61ea5fcf22286d55f908
Marcel Laverdet <marcel@laverdet.com>
2025-04-28 23:24:05 -0500

    Workaround gcc "sorry"
    It's been more than 15 years, I don't think it's on the roadmap.
    
      sorry, unimplemented: unexpanded parameter pack in local class in
      lambda

--- packages/utility/tuple.cc
+++ packages/utility/tuple.cc
@@ -12,17 +12,19 @@ namespace util {
 // could accept the underlying `construct` object. But who would do such a thing?
 // Adapted from this solution into a generic utility:
 // https://stackoverflow.com/questions/56187926/how-to-construct-a-tuple-of-non-movable-non-copyable-objects/56188636#56188636
+template <std::invocable<> Invocable>
+struct construct_type {
+		using result_type = std::invoke_result_t<Invocable>;
+		// NOLINTNEXTLINE(google-explicit-constructor)
+		constexpr operator result_type() const { return invocable(); }
+		Invocable invocable;
+};
+
 export template <std::invocable<>... Invocables>
 constexpr auto make_tuple_in_place(Invocables... invocables) {
 	return std::tuple<std::invoke_result_t<Invocables>...>{
 		std::invoke([ & ]() constexpr {
-			using result_type = std::invoke_result_t<Invocables>;
-			struct construct {
-					// NOLINTNEXTLINE(google-explicit-constructor)
-					constexpr operator result_type() const { return invocable(); }
-					Invocables invocable;
-			};
-			return construct{.invocable = std::move(invocables)};
+			return construct_type<Invocables>{.invocable = std::move(invocables)};
 		})...
 	};
 }
