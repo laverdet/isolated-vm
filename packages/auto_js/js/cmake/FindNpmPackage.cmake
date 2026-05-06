594780ef4936c921666b42c74c92c73d2a26c440
Marcel Laverdet <marcel@laverdet.com>
2026-05-06 02:08:41 -0500

    Restructure cmake subdirectory imports w/ npm
    I'll need to import v8_js elsewhere, so some movement here is needed.

--- packages/auto_js/js/cmake/FindNpmPackage.cmake
+++ packages/auto_js/js/cmake/FindNpmPackage.cmake
@@ -1,22 +1,29 @@
 # Instruct `find_package` to find listed npm packages
 include_guard(GLOBAL)
-set(NPM_INCLUDED_PACKAGES CACHE INTERNAL "")
-function(find_package_from_npm PACKAGE)
+set(NPM_INCLUDED_PACKAGE_NAMES CACHE INTERNAL "")
+function(find_package_from_npm PACKAGE_NAME)
+	# Optional parameter to override the deduplicated package name, allowing multiple includes of the
+	# same package.
+	set(NPM_PACKAGE_NAME "${PACKAGE_NAME}")
+	if(DEFINED ARGV1)
+		set(PACKAGE_NAME "${ARGV1}")
+	endif()
+
 	# Don't include more than once
-	if(PACKAGE IN_LIST NPM_INCLUDED_PACKAGES)
+	if(PACKAGE_NAME IN_LIST NPM_INCLUDED_PACKAGE_NAMES)
 		return()
 	endif()
-	set(NPM_INCLUDED_PACKAGES ${NPM_INCLUDED_PACKAGES} "${PACKAGE}" CACHE INTERNAL "")
+	set(NPM_INCLUDED_PACKAGE_NAMES ${NPM_INCLUDED_PACKAGE_NAMES} "${PACKAGE_NAME}" CACHE INTERNAL "")
 
 	# Search and include
 	execute_process(
-		COMMAND sh -c "cd \"${CMAKE_CURRENT_SOURCE_DIR}\"; node -e 'console.log(require.resolve(\"${PACKAGE}/CMakeLists.txt\"))'"
-		OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE PACKAGE_LOCATION
+		COMMAND sh -c "cd \"${CMAKE_CURRENT_SOURCE_DIR}\"; node -e 'console.log(require.resolve(\"${NPM_PACKAGE_NAME}/CMakeLists.txt\"))'"
+		OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE PACKAGE_NAME_LOCATION
 		RESULT_VARIABLE RESULT)
 	if(NOT RESULT STREQUAL 0)
-		message(FATAL_ERROR "Could not find npm package '${PACKAGE}'")
+		message(FATAL_ERROR "Could not find npm package '${NPM_PACKAGE_NAME}'")
 	endif()
-	get_filename_component(PACKAGE_LOCATION "${PACKAGE_LOCATION}" DIRECTORY)
-	message(STATUS "npm package '${PACKAGE}' found at '${PACKAGE_LOCATION}'")
-	add_subdirectory("${PACKAGE_LOCATION}" "${CMAKE_BINARY_DIR}/_npm/${PACKAGE}")
+	get_filename_component(PACKAGE_NAME_LOCATION "${PACKAGE_NAME_LOCATION}" DIRECTORY)
+	message(STATUS "npm package '${NPM_PACKAGE_NAME}' found at '${PACKAGE_NAME_LOCATION}'")
+	add_subdirectory("${PACKAGE_NAME_LOCATION}" "${CMAKE_BINARY_DIR}/_npm/${PACKAGE_NAME}")
 endfunction()
