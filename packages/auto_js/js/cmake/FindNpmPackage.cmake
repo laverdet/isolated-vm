0b21a784c37f2d929ce7ea9dd876a650d84859c7
Marcel Laverdet <marcel@laverdet.com>
2026-06-09 11:43:11 -0500

    Support dual auto_js consumers
    You might have the situation where you want to link auto_js against
    isolated_vm (for NativeModule), which will import js::error's typeinfo.
    You also might want to use @auto_js/napi on its own, which builds its
    own js::error typeinfo. You might also want to do both of those things
    in the same cmake project (with different targets obviously).

--- packages/auto_js/js/cmake/FindNpmPackage.cmake
+++ packages/auto_js/js/cmake/FindNpmPackage.cmake
@@ -1,29 +1,57 @@
 # Instruct `find_package` to find listed npm packages
 include_guard(GLOBAL)
-set(NPM_INCLUDED_PACKAGE_NAMES CACHE INTERNAL "")
-function(find_package_from_npm PACKAGE_NAME)
+
+# set_npm_package_properties([variables...])
+function(set_npm_package_properties PACKAGE_NAME)
+	# Inherit `NPM_PACKAGE_NAME` from `find_package_from_npm` scope
+	if(DEFINED NPM_PACKAGE_NAME)
+		set(PACKAGE_NAME "${NPM_PACKAGE_NAME}")
+	endif()
+
+	# Save variables as global properties
+	list(POP_FRONT ARGV) # PACKAGE_NAME
+	foreach(VAR IN LISTS ARGV)
+		set_property(GLOBAL PROPERTY "NPM_PACKAGE:${PACKAGE_NAME}:${VAR}" "${${VAR}}")
+	endforeach()
+endfunction()
+
+# find_package_from_npm(name [AS identifier] [PROPAGATE variables...])
+function(find_package_from_npm NPM_PACKAGE_NAME)
+	# Parse arguments:
+	cmake_parse_arguments(PARSE_ARGV 1 npm "" "AS" "PROPAGATE")
+
 	# Optional parameter to override the deduplicated package name, allowing multiple includes of the
 	# same package.
-	set(NPM_PACKAGE_NAME "${PACKAGE_NAME}")
-	if(DEFINED ARGV1)
-		set(PACKAGE_NAME "${ARGV1}")
+	set(NPM_PACKAGE_NAME_REAL "${NPM_PACKAGE_NAME}")
+	if(DEFINED npm_AS)
+		set(NPM_PACKAGE_NAME "${npm_AS}")
+		unset(npm_AS)
 	endif()
 
-	# Don't include more than once
-	if(PACKAGE_NAME IN_LIST NPM_INCLUDED_PACKAGE_NAMES)
-		return()
-	endif()
-	set(NPM_INCLUDED_PACKAGE_NAMES ${NPM_INCLUDED_PACKAGE_NAMES} "${PACKAGE_NAME}" CACHE INTERNAL "")
+	# Check against global include list
+	get_property(NPM_INCLUDED_PACKAGE_NAMES GLOBAL PROPERTY NPM_INCLUDED_PACKAGE_NAMES)
+	if(NOT NPM_PACKAGE_NAME IN_LIST NPM_INCLUDED_PACKAGE_NAMES)
+		# It was not included yet. Mark as included, and `add_subdirectory`
+		list(APPEND NPM_INCLUDED_PACKAGE_NAMES "${NPM_PACKAGE_NAME}")
+		set_property(GLOBAL PROPERTY NPM_INCLUDED_PACKAGE_NAMES ${NPM_INCLUDED_PACKAGE_NAMES})
 
-	# Search and include
-	execute_process(
-		COMMAND sh -c "cd \"${CMAKE_CURRENT_SOURCE_DIR}\"; node -e 'console.log(require.resolve(\"${NPM_PACKAGE_NAME}/CMakeLists.txt\"))'"
-		OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE PACKAGE_NAME_LOCATION
-		RESULT_VARIABLE RESULT)
-	if(NOT RESULT STREQUAL 0)
-		message(FATAL_ERROR "Could not find npm package '${NPM_PACKAGE_NAME}'")
+		# Search and include
+		execute_process(
+			COMMAND sh -c "cd \"${CMAKE_CURRENT_SOURCE_DIR}\"; node -e 'console.log(require.resolve(\"${NPM_PACKAGE_NAME_REAL}/CMakeLists.txt\"))'"
+			OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE PACKAGE_NAME_LOCATION
+			RESULT_VARIABLE RESULT)
+		if(NOT RESULT STREQUAL 0)
+			message(FATAL_ERROR "Could not find npm package '${NPM_PACKAGE_NAME_REAL}'")
+		endif()
+		get_filename_component(PACKAGE_NAME_LOCATION "${PACKAGE_NAME_LOCATION}" DIRECTORY)
+		message(STATUS "npm package '${NPM_PACKAGE_NAME_REAL}' found at '${PACKAGE_NAME_LOCATION}'")
+		add_subdirectory("${PACKAGE_NAME_LOCATION}" "${CMAKE_BINARY_DIR}/_npm/${NPM_PACKAGE_NAME}")
 	endif()
-	get_filename_component(PACKAGE_NAME_LOCATION "${PACKAGE_NAME_LOCATION}" DIRECTORY)
-	message(STATUS "npm package '${NPM_PACKAGE_NAME}' found at '${PACKAGE_NAME_LOCATION}'")
-	add_subdirectory("${PACKAGE_NAME_LOCATION}" "${CMAKE_BINARY_DIR}/_npm/${PACKAGE_NAME}")
+
+	# Propagate variables registered by set_npm_package_properties
+	foreach(VAR IN LISTS npm_PROPAGATE)
+		get_property(VALUE GLOBAL PROPERTY "NPM_PACKAGE:${NPM_PACKAGE_NAME}:${VAR}")
+		set(${VAR} "${VALUE}" PARENT_SCOPE)
+	endforeach()
+	unset(npm_PROPAGATE)
 endfunction()
